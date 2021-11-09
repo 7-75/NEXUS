84a57e2d10c5a5ec7002bfe3dac3ab3f72c53b4f
4kz <35037484+4kz@users.noreply.github.com>
2021-11-09 10:48:27 +0100

    Fixed NexusCreatedEvent;

--- Nexus Core/src/main/java/brawl/nexuscore/NexusController.java
+++ Nexus Core/src/main/java/brawl/nexuscore/NexusController.java
@@ -1,23 +1,24 @@
 package brawl.nexuscore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NexusController {
 
     public static JavaPlugin                        plugin;
     public static String                            nexusBlockMaterial;
     public static String                            nexusItemLore;
-    public static List<Location>                    nexusBlocks;
+    public static ArrayList<Location>               nexusBlocks;
+    public static ObjectMapper                      objectMapper;
 
     public NexusController(JavaPlugin plugin) {
-        NexusController.plugin      = plugin;
-        nexusBlockMaterial          = plugin.getConfig().getString("nexusBlockMaterial");
-        nexusItemLore               = plugin.getConfig().getString("nexusItemLore");
-        nexusBlocks                 = new ArrayList<>();
-
+        NexusController.plugin = plugin;
+        nexusBlockMaterial = plugin.getConfig().getString("nexusBlockMaterial");
+        nexusItemLore = plugin.getConfig().getString("nexusItemLore");
+        nexusBlocks = new ArrayList<>();
+        objectMapper = new ObjectMapper();
     }
 }
